d5aa50fee3302fc863023fff51f442e2725ed015
David A Koch <kodave1@gmail.com>
2015-06-18 15:06:23 -0400

    added java functionality to the donate button

---
+++ scripts/source.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+
+
+
+	$("#donate").click(function(){
+
+		alert("Thanks for Donating!")
+		});
+
+	$("#donateHeader").click(function(){
+
+		alert("Thanks for Donating!");
+		});
+
+
+
+
+});
