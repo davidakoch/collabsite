1aefe652d8e7853b304285b129286f53f6d07336
David A Koch <kodave1@gmail.com>
2015-06-29 22:12:36 -0400

    added an image slider

--- scripts/source.js
+++ scripts/source.js
@@ -14,7 +14,26 @@ $(document).ready(function() {
 
 	/*create the jquery slideshow*/
 
-	
+	//hide all the images except the first one
+	$(".photos img:gt(0)").hide();
+
+	setInterval (function() {
+		//get the current image - it is the visible one
+		var current = $(".photos img:visible");
+
+		//get immediate next image after the current if exist,
+		//otherwise find the first one
+
+		var next = current.next().length ? current.next() : $(".photos img:eq(0)");
+		//hide the current image
+		current.fadeOut (2000);
+		//show the next one
+		next.fadeIn(2000);
+
+
+	}, 3000);
+
+
 
 
 
